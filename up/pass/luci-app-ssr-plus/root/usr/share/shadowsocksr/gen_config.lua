e9f3097a8a1cafb4ee5a59b4dd6b0edf33c340f3
loso3000 sir <51484662+loso3000@users.noreply.github.com>
2025-07-07 09:22:41 +0800

    p

--- up/pass/luci-app-ssr-plus/root/usr/share/shadowsocksr/gen_config.lua
+++ up/pass/luci-app-ssr-plus/root/usr/share/shadowsocksr/gen_config.lua
@@ -412,14 +412,29 @@ local ss = {
 	reuse_port = true
 }
 local hysteria2 = {
-	server = (server.server_port and (server.port_range and (server.server .. ":" .. server.server_port .. "," .. string.gsub(server.port_range, ":", "-")) or (server.server .. ":" .. server.server_port) or (server.port_range and server.server .. ":" .. string.gsub(server.port_range, ":", "-") or server.server .. ":443"))),
+	server = (
+		server.server_port and 
+		(
+			server.port_range and 
+			(server.server .. ":" .. server.server_port .. "," .. string.gsub(server.port_range, ":", "-")) 
+			or 
+			(server.server .. ":" .. server.server_port)
+		) 
+		or 
+		(
+			server.port_range and 
+			server.server .. ":" .. string.gsub(server.port_range, ":", "-") 
+			or 
+			server.server .. ":443"
+		)
+	),
 	bandwidth = (server.uplink_capacity or server.downlink_capacity) and {
 	up = tonumber(server.uplink_capacity) and tonumber(server.uplink_capacity) .. " mbps" or nil,
 	down = tonumber(server.downlink_capacity) and tonumber(server.downlink_capacity) .. " mbps" or nil 
 	} or nil,
 	socks5 = (proto:find("tcp") and tonumber(socks_port) and tonumber(socks_port) ~= 0) and {
 		listen = "0.0.0.0:" .. tonumber(socks_port),
-		disable_udp = false
+		disableUDP = false
 	} or nil,
 	transport = server.transport_protocol and {
 		type = server.transport_protocol or "udp",
@@ -452,7 +467,7 @@ local hysteria2 = {
 		disablePathMTUDiscovery = (server.disablepathmtudiscovery == "1") and true or false
 	} or nil,
 	auth = server.hy2_auth,
-	tls = (server.tls_host) and {
+	tls = server.tls_host and {
 		sni = server.tls_host,
 		--alpn = server.tls_alpn or nil,
 		insecure = (server.insecure == "1") and true or false,
@@ -495,7 +510,7 @@ local chain_sslocal = {
 	} or {{ 
 			protocol = "socks",
 			local_address = "0.0.0.0",
-			ocal_port = tonumber(socks_port)
+			local_port = tonumber(socks_port)
 			}},
 		servers = {
 			{
