411638813c5c2a182af1adce0950365e8b3845c8
loso3000 sir <51484662+loso3000@users.noreply.github.com>
2025-07-08 16:33:19 +0800

    jp

--- up/tool/luci-app-ota/luasrc/controller/admin/ota.lua
+++ up/tool/luci-app-ota/luasrc/controller/admin/ota.lua
@@ -108,7 +108,6 @@ function action_ota()
             end
             os.execute("gzip -dc " .. image_tmp .. " > " .. image_extracted) 
 
-
             -- 处理分区扩展
             os.execute("echo 'Expanding partition...' >> /etc/ezotaflash.log")
             local sizes = {0, 1024, 2048, 5120, 10240, 20480}  
@@ -119,83 +118,88 @@ function action_ota()
 	    
 	    fork_exec("(sgdisk -e " .. image_extracted .. " >/dev/null 2>&1; true)")
 	    fork_exec("(echo -e 'resizepart 2 -1\\nquit' | parted " .. image_extracted .. "  >/dev/null 2>&1; true)")
-	    
-	    os.execute("echo 'Starting DD flash process...' >> /etc/ezotaflash.log")
+	         
+            local script = string.format([[
+            #!/bin/sh
+	    sleep 1
+            echo "[$(date)] Starting DD flash ..." >> /etc/ezotaflash.log
+	    sleep 2
+            killall -9 dropbear uhttpd nginx
+	    sleep 1
+            dd if=%s of=%s bs=4k conv=fsync status=progress >> /etc/ezotaflash.log 2>&1
+            sync
+            echo "[$(date)] END DD flash" >> /etc/ezotaflash.log
+	    sleep 3
+            echo b > /proc/sysrq-trigger
+            ]], image_extracted, image_extracteddev)
+ 
+            local f = io.open("/tmp/otaflash.sh", "w")
+            f:write(script)
+            f:close()
+            os.execute("chmod 755 /tmp/otaflash.sh")
 
-
-             local cmd = string.format(
-               "sleep 2; killall dropbear uhttpd nginx;  sleep 1; sync; (dd if=%s of=%s bs=4k conv=fsync) && echo 'Rebooting system...' >> /etc/ezotaflash.log ",
-	       image_extracted,
-               image_extracteddev
-            )
-	    
-            -- 执行升级
-            -- fork_exec(cmd)
-
-	    fork_exec("(dd if=" .. image_extracted .. "  of=" .. image_extracteddev .. " bs=4k conv=fsync >/dev/null 2>&1) && echo 'DD flash END' >> /etc/ezotaflash.log ")
-	    os.execute("echo 'END' >> /etc/ezotaflash.log")
+            fork_exec("/bin/sh /tmp/otaflash.sh")
 
         else
 	    os.execute("echo 'Starting sysupgrade process...' >> /etc/ezotaflash.log")
             fork_exec("sleep 1; killall dropbear uhttpd nginx; sleep 1; sync; (/sbin/sysupgrade %s %q ) && echo 'Sysupgrade END' >> /etc/ezotaflash.log " %{ slist, image_tmp })
         end
 
-  else
-    luci.template.render("admin_system/ota")
-  end
-   
+    else
+        luci.template.render("admin_system/ota")
+    end
 end
 
 function action_check()
     local r, o, e = ota_exec("ota check")
     local ret = {
-    code = 500,
-    msg = "Unknown"
-  }
-  if r == 0 or r == 1 or r == 2 then
-    ret.code = r
-    ret.msg = o
-  else
-    ret.code = 500
-    ret.msg = e
-  end
-  luci.http.prepare_content("application/json")
+        code = 500,
+        msg = "Unknown"
+    }
+    if r == 0 or r == 1 or r == 2 then
+        ret.code = r
+        ret.msg = o
+    else
+        ret.code = 500
+        ret.msg = e
+    end
+    luci.http.prepare_content("application/json")
     luci.http.write_json(ret)
 end
 
 function action_download()
     local r, o, e = ota_exec("ota download")
-  local ret = {
-    code = 500,
-    msg = "Unknown"
-  }
-  if r == 0 then
-    ret.code = 0
-    ret.msg = ""
-  else
-    ret.code = 500
-    ret.msg = e
-  end
-  luci.http.prepare_content("application/json")
-  luci.http.write_json(ret)
+    local ret = {
+        code = 500,
+        msg = "Unknown"
+    }
+    if r == 0 then
+        ret.code = 0
+        ret.msg = ""
+    else
+        ret.code = 500
+        ret.msg = e
+    end
+    luci.http.prepare_content("application/json")
+    luci.http.write_json(ret)
 end
 
 function action_progress()
     local r, o, e = ota_exec("ota progress")
     local ret = {
-    code = 500,
-    msg = "Unknown"
-  }
-  if r == 0 then
-    ret.code = 0
-    ret.msg = "done"
-  elseif r == 1 or r == 2 or r == 254 then
-    ret.code = r
-    ret.msg = o
-  else
-    ret.code = 500
-    ret.msg = e
-  end
+        code = 500,
+        msg = "Unknown"
+    }
+    if r == 0 then
+        ret.code = 0
+        ret.msg = "done"
+    elseif r == 1 or r == 2 or r == 254 then
+        ret.code = r
+        ret.msg = o
+    else
+        ret.code = 500
+        ret.msg = e
+    end
     luci.http.prepare_content("application/json")
     luci.http.write_json(ret)
 end
@@ -203,40 +207,40 @@ end
 function action_cancel()
     local r, o, e = ota_exec("ota cancel")
     local ret = {
-    code = 500,
-    msg = "Unknown"
-  }
-  if r == 0 then
-    ret.code = 0
-    ret.msg = "ok"
-  else
-    ret.code = 500
-    ret.msg = e
-  end
+        code = 500,
+        msg = "Unknown"
+    }
+    if r == 0 then
+        ret.code = 0
+        ret.msg = "ok"
+    else
+        ret.code = 500
+        ret.msg = e
+    end
     luci.http.prepare_content("application/json")
     luci.http.write_json(ret)
 end
 
 function fork_exec(command)
-	local pid = nixio.fork()
-	if pid > 0 then
-		return
-	elseif pid == 0 then
-		-- change to root dir
-		nixio.chdir("/")
+    local pid = nixio.fork()
+    if pid > 0 then
+        return
+    elseif pid == 0 then
+        -- change to root dir
+        nixio.chdir("/")
 
-		-- patch stdin, out, err to /dev/null
-		local null = nixio.open("/dev/null", "w+")
-		if null then
-			nixio.dup(null, nixio.stderr)
-			nixio.dup(null, nixio.stdout)
-			nixio.dup(null, nixio.stdin)
-			if null:fileno() > 2 then
-				null:close()
-			end
-		end
+        -- patch stdin, out, err to /dev/null
+        local null = nixio.open("/dev/null", "w+")
+        if null then
+            nixio.dup(null, nixio.stderr)
+            nixio.dup(null, nixio.stdout)
+            nixio.dup(null, nixio.stdin)
+            if null:fileno() > 2 then
+                null:close()
+            end
+        end
 
-		-- replace with target command
-		nixio.exec("/bin/sh", "-c", command)
-	end
+        -- replace with target command
+        nixio.exec("/bin/sh", "-c", command)
+    end
 end
