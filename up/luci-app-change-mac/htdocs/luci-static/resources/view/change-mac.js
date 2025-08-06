2ce003dd58fc3b8ed7bd7d6f312bcd733fa3a5b4
loso3000 sir <51484662+loso3000@users.noreply.github.com>
2025-08-06 21:29:07 +0800

    u

---
+++ up/luci-app-change-mac/htdocs/luci-static/resources/view/change-mac.js
@@ -0,0 +1,51 @@
+'use strict';
+'require form';
+'require network';
+'require uci';
+'require view';
+'require tools.widgets as widgets';
+
+return view.extend({
+	load: function() {
+		return Promise.all([
+			uci.changes(),
+			uci.load('change-mac')
+		]);
+	},
+
+	render: function(data) {
+
+
+		let m, s, o;
+
+		m = new form.Map('change-mac', _('MAC address randomizer'),
+			_('Assign a random MAC address to the designated interface on every time boot'));
+
+		s = m.section(form.TypedSection, 'change-mac');
+		s.anonymous = true;
+
+		o = s.option(form.Flag, 'enabled', _('Enable MAC randomization'));
+		o.rmempty = false;
+
+		o = s.option(form.Value, 'delaytime', _('delay time'));	
+		o.datatype = 'and(uinteger,min(1))';
+		o.rmempty = false;
+		
+		o = s.option(widgets.DeviceSelect, 'interface', _('Enabled interfaces'));
+		o.multiple = true;
+		o.noaliases = true;
+		o.nobridges = true;
+		o.nocreate = true;
+
+
+		o = s.option(form.Flag, 'arp_drop', _('DROP ARP'));
+		o.rmempty = false;
+
+		o = s.option(form.Flag, 'ping_drop', _('DROP PING'));
+		o.rmempty = false;
+		
+
+
+		return m.render();
+	}
+});
