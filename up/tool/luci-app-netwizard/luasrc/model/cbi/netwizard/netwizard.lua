7dfe71c4faa6fcf046c30d88d423cfeaf6eb5a42
loso3000 sir <51484662+loso3000@users.noreply.github.com>
2025-08-19 17:06:44 +0800

    xx

--- up/tool/luci-app-netwizard/luasrc/model/cbi/netwizard/netwizard.lua
+++ up/tool/luci-app-netwizard/luasrc/model/cbi/netwizard/netwizard.lua
@@ -154,6 +154,12 @@ e:value("1.1.1.1", translate("Cloudflare DNS:1.1.1.1"))
 e.anonymous = false
 e:depends("dnsset", true)
 
+e = s:taboption("wansetup", Flag, "forwarding", translate("Forcefully forwarding"),translate("Forcefully add LAN to WAN forwarding"))
+e.default = "1"
+e:depends({wan_proto="pppoe"})
+e:depends({wan_proto="dhcp"})
+e:depends({wan_proto="static"})
+e.anonymous = false
 if has_wifi then
 	e = s:taboption("wifisetup", Value, "wifi_ssid", translate("<abbr title=\"Extended Service Set Identifier\">ESSID</abbr>"))
 	e.datatype = "maxlength(32)"
