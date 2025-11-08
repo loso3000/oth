96b8546bad414a0558f8e0f6340478ad1d709ad4
loso3000 sir <51484662+loso3000@users.noreply.github.com>
2025-11-08 16:29:47 +0800

    f

--- up/tool/luci-app-netwizard/luasrc/model/cbi/netwizard/netwizard.lua
+++ up/tool/luci-app-netwizard/luasrc/model/cbi/netwizard/netwizard.lua
@@ -246,7 +246,7 @@ if has_wifi then
 end
 
 e = s:taboption("wansetup", Flag, 'https', translate('Enables SSL secure access'))
-e.default = 0
+e.default = 1
 e.anonymous = false
 
 -- e = s:taboption("wansetup", DummyValue, 'ssl_cert', translate('SSL cert'))
